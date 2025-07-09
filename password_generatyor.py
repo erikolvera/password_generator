1722889938f57bb29a6c133be40d158d8d2f39c9
Erik Olvera <erikolvera44@gmail.com>
2025-07-08 21:10:36 -0500

    edited password_length prompt and added an if-elif-else statement to handle the logic

--- password_generatyor.py
+++ password_generatyor.py
@@ -3,10 +3,16 @@ import random
 #all possible characters the password could be
 possible_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_"
 
-#prompt the user a message asking them how long they want their password to be
-password_length = input("What password length would you like? MAX OF 16 CHARACTERS")
+#prompt the user a message asking them how long they want their password to be (max of 16)
+password_length = int(input("Enter desired password length (MAX 16): "))
 
-
-password = ''.join(random.choice(possible_characters) for _ in range(16))
-
-print(password)
+# prints this message out if the user wants a password over 16 characters
+if password_length > 16:
+    print("Password length must be less than 16")
+# elif statement if the user inputs 0 or a negative number
+elif password_length <= 0:
+    print("Please enter a number 1-16")
+# generates the password if all conditions are satisfactory
+else:
+    password = ''.join(random.choice(possible_characters) for _ in range(password_length))
+    print(password)
