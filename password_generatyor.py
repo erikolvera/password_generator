9fee926bf165499745efe4a82093d4e18f27e4e5
Erik Olvera <erikolvera44@gmail.com>
2025-07-08 20:24:48 -0500

    added a list of possible characters and to print a password length of 16

--- password_generatyor.py
+++ password_generatyor.py
@@ -1,1 +1,7 @@
 import random
+
+possible_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_"
+
+password = ''.join(random.choice(possible_characters) for _ in range(16))
+
+print(password)
