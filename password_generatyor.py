71bd488307ad31938aa5bef5521be0752bc77b5c
Erik Olvera <erikolvera44@gmail.com>
2025-07-08 20:35:58 -0500

    added a user prompt asking them to input the password length they want

--- password_generatyor.py
+++ password_generatyor.py
@@ -1,6 +1,11 @@
 import random
 
+#all possible characters the password could be
 possible_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_"
+
+#prompt the user a message asking them how long they want their password to be
+password_length = input("What password length would you like? MAX OF 16 CHARACTERS")
+
 
 password = ''.join(random.choice(possible_characters) for _ in range(16))
 
